977776a25cbde9d4cacf60d8f64225d5d9138f65
vladislavo <rykovinternational@gmail.com>
2018-05-24 13:02:53 +0200

    nums

---
+++ ss_server/sendShares.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mail=0
+emails_f="emails"
+
+# Preparamos la lista de correos
+IFS=','
+emails=($emails_f)
+
+for i in "shares/"
+do
+	echo "Su compartición está adjunta a este correo." | mutt -a $i -s "Secreto compartido" --${emails[$mail]}
+	mail=$[mail+1]
+done
